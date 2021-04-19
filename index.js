6baebae885704a10b2fac40a7bdce87ad2521e75
Dima7859 <68110092+Dima7859@users.noreply.github.com>
2021-04-19 20:58:20 +0300

    DZ_16

--- index.js
+++ index.js
@@ -1,264 +1,242 @@
 //!!!!!!!!!!!!exercise1!!!!!!!!!!!!!!
 
 
-function newElement() {
-    let li = document.createElement("li");
-    let inputValue = document.getElementById("myInput").value;
-    let x = inputValue.split('');
-    let a = [];
+const arr = [213, 4, 5, 8, 3, 1, 85, 52];
 
-    for (let i = 0; i < x.length; i++) {
-        if ((x[i] % 2 !== 0) && (x[i + 1] % 2 !== 0)) {
-            if (i !== x.length - 1) {
-                a.push(x[i]);
-                a.push(':');
-            } else {
-                a.push(x[i]);
-            }
-        } else {
-            a.push(x[i]);
-        }
-    }
-
-    a = a.join('');
-    let t = document.createTextNode(a);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("Вы должны что-то написать!");
-    } else {
-      document.getElementById("myUL").prepend(li);
-    }
-    document.getElementById("myInput").value = "";
-
+function ubSortArr(ver) {
+    const newArr = ver ;
+    return newArr.sort((a, b) => a < b ? 1 : -1);
 }
+
+console.log(ubSortArr(arr));
 
 
 //!!!!!!!!!!!!exercise2!!!!!!!!!!!!!!
 
 
-let newArray = ['123' , '3645', 'Alex', 'Dima', 'Diana', 'Den', '54375', '365', '364555'];
+const arr2 = [213, false, 4, 5, NaN, 8, 3, '', undefined, true, 1, 0, -0, 85, 52, false, '', undefined, NaN, 0,];
 
-//!!!!!!!!!!!!version1!!!!!!!!!!!!!!
-
-function getFirst(array, n) {
-    let arrall = array;
-    let minarr = [];
-    let rezult = [];
-    for (let i = 0; i < arrall.length; i++) {
-        minarr = arrall[i].split('');
-        if (minarr[0].toLowerCase() == n.toLowerCase()) {
-            rezult.push(arrall[i]);
-        }
-    }
-
-    return rezult;
+function filterFalse(arr) {
+    return arr.filter(item => item)
 }
 
-//!!!!!!!!!!!!version2!!!!!!!!!!!!!!
+console.log(filterFalse(arr2));
 
-//!!!! тут я попытался сделать поиск по превым ведённым символам , вроде как получилось!!!!!!!!!!
-
-function getFirst2(array, n) {
-    let arrall = array;
-    let minarr = [];
-    let rezult = [];
-    let newN = n.split('');
-    let newNlenght = newN.length;
-    for (let i = 0; i < arrall.length; i++) {
-        minarr = arrall[i].split('');
-        let sovpad = 0;
-        for (let f = 0; f < newNlenght; f++) {
-            let micser = newN[f];
-            if (!/\D/.test(micser)) {
-                if (minarr[f] == newN[f]) {
-                    sovpad++ ;
-                }
-            } else {
-                if (minarr[f].toLowerCase() == newN[f].toLowerCase()) {
-                    sovpad++ ;
-                }
-            }
-        }
-        if (sovpad >= newNlenght) {
-            rezult.push(arrall[i]);
-        }
-    }
-
-    return rezult;
-}
 
 
 //!!!!!!!!!!!!exercise3!!!!!!!!!!!!!!
 
 
-function getLast(array, n) {
-    let arrall = array;
-    let minarr = [];
-    let rezult = [];
-    for (let i = 0; i < arrall.length; i++) {
-        minarr = arrall[i].split('');
-        if (minarr[minarr.length - 1].toLowerCase() == n.toLowerCase()) {
-            rezult.push(arrall[i]);
-        }
-    }
+const user1 = {
+    firsName:'Vika',
+    lastName:'Rezer',
+    age: 45
+};
 
-    return rezult;
+const user2 = {
+    firsName:'Aleks',
+    lastName:'Amd',
+    age: 18,
+};
+
+const user3 = {
+    firsName:'Diana',
+    lastName:'Nvidia',
+    age: 5,
+};
+
+const userall = [
+    user1,
+    user2,
+    user3
+];
+
+
+// const name_key = prompt('Введите ключ...'); 
+
+const name_key = 'firsName';
+
+function propertyValue(array, key) {
+    return array.map(item => item[key]);
 }
 
-console.log(getFirst(newArray, '3'));
-
-console.log(getFirst2(newArray, '36455'));
-
-console.log(getLast(newArray, 'a'));
+console.log(propertyValue(userall, name_key));
 
 
-//!!!!!!!!!!!!exercise4!!!!!!!!!!!!!!
+//!!!!!!!!!!!!exercise4 and exercise6!!!!!!!!!!!!!!
 
 
-const arr1 = [123 , 456523, '@', 'ygfr', '%'];
-const arr2 = [123 , 'gyhewr', '%', 65743];
+const arrstr = ['Diana', 'Anna', 'Mervyn', 'Jack', 'Ariel', 'Sophie'];
 
-////!!!!!!!!!!!!!!!!!My version!!!!!!!!!!!!!!!!!
 
-function arrayes(var1, var2) {
-    let arrall = var1.concat(var2)
-    let unique = [];
-    let yes = 0;
-    for (let i = 0; i < arrall.length; i++) {
-        for (let a = 0; a < arrall.length; a++) {
-            if (arrall[i] == arrall[a]) {
-                yes++ ;
-            }
-        }
+const lengthArrForEach = [];
 
-        if (yes == 1) {
-            unique.push(arrall[i])
-        }
-        yes = 0;
-    }
-    return unique;
-}
+arrstr.forEach(item => lengthArrForEach.push(item.length));
+console.log('Через forEach');
+console.log(lengthArrForEach);
 
-//!!!!!!!!!!!!!!!!!!!!Internet!!!!!!!!!!!!!!!!!!
+const lengthArrReduce = arrstr.reduce((acc, item) => ([...acc, item.length]), []);
+console.log('Через reduce');
+console.log(lengthArrReduce);
 
-function arrayes2(var1, var2) {
-    let arrall = var1.concat(var2)
-    let unique = [];
-    for (var i=0, l=arrall.length; i<l; i++)
-    if (unique.indexOf(arrall[i]) === -1)
-        unique.push(arrall[i]);
-    return unique;
-}
 
-console.log(arrayes(arr1, arr2));
-
-console.log(arrayes2(arr1, arr2));
+const lengthArrMap = arrstr.map(item => item.length );
+console.log('Через map');
+console.log(lengthArrMap);
 
 
 //!!!!!!!!!!!!exercise5!!!!!!!!!!!!!!
 
 
-let sum = [2, 7, 3, 5];
+const numbers = [2, 3, 5, 7, 11, 13, 17, 19];
 
-function sumOfSquares(arr) {
-    let x = 0;
-
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = Math.pow(arr[i], 2);
-        x += arr[i]
-    }
-
-    return x
-}
-
-console.log(sumOfSquares(sum));
-
-
-// !!!!!!!!!!!!exercise6!!!!!!!!!!!!!!
-
-
-let user = [
-    {
-        name:'Vika',
-        age: 45,
-        job: 'It'
-    },
-    {
-        name:'Aleks',
-        age: 18,
-        job: 'It'
-    },
-    {
-        name:'Diana',
-        age: 85,
-        job: 'pensioner'
-    },
-    {
-        name:'Dima',
-        age: 12,
-        job: 'school'
-    }
-];
-
-let skey = prompt('Введите по какому ключу сортировать');
-
-
-//!!!!!!!!!!!!version1!!!!!!!!!!!!!!
-
-
-function sor(arr, s) {
-    if (s in arr[0]) {
-        for (let i = 0, endI = arr.length - 1; i < endI; i++) {
-            for (let j = 0, endJ = endI - i; j < endJ; j++) {
-                if (arr[j][s] > arr[j + 1][s]) {
-                    let swap = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = swap;
-                }
-            }
+function currentSums(numbers) {
+    return newNumbers = numbers.reduce((acc, item, i) => {
+        if (i == 0) {
+            acc.push(item);
+            return acc;
+        } else {
+            acc.push(acc[i - 1] + item);
+            return acc;
         }
-        return arr;
-    }else {
-        return 'такого ключа нет';
-    }
+}, []);
 }
 
-console.log(sor(user, skey));
 
-
-//!!!!!!!!!!!!version2!!!!!!!!!!!!!!
-
-
-function sortByAge(arr, s) {
-    if (s in arr[0]) {
-        return arr.sort((a, b) => a[s] > b[s] ? 1 : -1);
-    }else {
-        return 'такого ключа нет';
-    }
-}
-
-console.log(sortByAge(user, skey));
+console.log(currentSums(numbers));
 
 
 //!!!!!!!!!!!!exercise7!!!!!!!!!!!!!!
 
 
-let k = prompt('Введите ключ...');
+let ver1 = [];
+let ver2 = [];
 
-function propertyValue(array, key) {
-    key = key.toLowerCase();
-    let res = [];
-    if (key in array[0]) {
-        for (let i = 0; i < array.length; i++) {
-            res.push(array[i][key]);
-        }
-        // res = res.join(', ');
-        return res;
-    }else {
-        return 'такого ключа нет';
+var myNodelist = document.getElementsByTagName("LI");
+for (let i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
     }
 }
 
 
-console.log(propertyValue(user, k));
+function newElement() {
+    let inputValue = document.getElementById("myInput").value;
+    let inputValuePush;
+    if (inputValue !== '') {
+        if (!/\D/.test(inputValue)) {
+            inputValuePush = +inputValue;
+            ver1.push(inputValuePush);
+        }else {
+            ver2.push(inputValue);
+        }
+    }
+    if (inputValue === '') {
+      alert("Вы должны что-то написать!");
+    }
+    document.getElementById("myInput").value = "";
+    return ver1,ver2;
+}
 
+
+function sorarr() {
+    console.log(ver2);
+    let x = ver1;
+    let y = ver2;
+    let li = document.createElement("li");
+    let li2 = document.createElement("li");
+    let t;
+    let c;
+
+    if (x.length !== 0 && y.length == 0) {
+        x.sort((a, b) => a > b ? 1 : -1)
+        x = x.join(', ');
+        t = document.createTextNode(x);
+        li.appendChild(t);
+        document.getElementById("myUL").prepend(li);
+        document.getElementById("myInput").value = "";
+        ver1 = [];
+
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+      
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function() {
+                var div = this.parentElement;
+                div.style.display = "none";
+            }
+        }
+
+        return ver1;
+    } else if (y.length !== 0 && x.length == 0) {
+        y.sort((a, b) => a > b ? 1 : -1)
+        y = y.join(', ');
+        t = document.createTextNode(y);
+        li.appendChild(t);
+        document.getElementById("myUL").prepend(li);
+        document.getElementById("myInput").value = "";
+        ver2 = [];
+
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+      
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function() {
+                var div = this.parentElement;
+                div.style.display = "none";
+            }
+        }
+
+        return ver2;
+    } else if (y.length !== 0 && x.length !== 0) {
+        x.sort((a, b) => a > b ? 1 : -1)
+        x = x.join(', ');
+        y.sort((a, b) => a > b ? 1 : -1)
+        y = y.join(', ');
+        t = document.createTextNode(x);
+        c = document.createTextNode(y);
+        li.appendChild(t);
+        li2.appendChild(c);
+        document.getElementById("myUL").prepend(li);
+        document.getElementById("myUL").prepend(li2);
+        document.getElementById("myInput").value = "";
+        ver1 = [];
+        ver2 = [];
+
+        var span = document.createElement("SPAN");
+        var span2 = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        var txt2 = document.createTextNode("\u00D7");
+        span.className = "close";
+        span2.className = "close";
+        span.appendChild(txt);
+        span2.appendChild(txt2);
+        li.appendChild(span);
+        li2.appendChild(span2);
+    
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function() {
+                var div = this.parentElement;
+                div.style.display = "none";
+            }
+        }
+
+        return ver1, ver2;
+    }
+}
