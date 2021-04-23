7b279af686a6459a5bfed7056fb7381379784521
Dima7859 <68110092+Dima7859@users.noreply.github.com>
2021-04-23 20:09:46 +0300

    DZ_17

--- index.js
+++ index.js
@@ -1,242 +1,136 @@
-//!!!!!!!!!!!!exercise1!!!!!!!!!!!!!!
+//__________exercise from 1 to 3________
 
 
-const arr = [213, 4, 5, 8, 3, 1, 85, 52];
+class Worker{
 
-function ubSortArr(ver) {
-    const newArr = ver ;
-    return newArr.sort((a, b) => a < b ? 1 : -1);
-}
+    #name;
+    #surname;
+    #rate;
+    #days;
 
-console.log(ubSortArr(arr));
+    get name(){
+        return this.#name
+    }
 
+    get surname(){
+        return this.#surname
+    }
 
-//!!!!!!!!!!!!exercise2!!!!!!!!!!!!!!
+    get rate(){
+        return this.#rate
+    }
 
+    get days(){
+        return this.#days
+    }
 
-const arr2 = [213, false, 4, 5, NaN, 8, 3, '', undefined, true, 1, 0, -0, 85, 52, false, '', undefined, NaN, 0,];
+    set rate(value) {
+        this.#rate = value;
+    }
 
-function filterFalse(arr) {
-    return arr.filter(item => item)
-}
+    set days(value) {
+        this.#days = value;
+    }
 
-console.log(filterFalse(arr2));
+    constructor (name, surname, rate, days){
+        this.#name = name;
+        this.#surname = surname;
+        this.#rate = rate;
+        this.#days = days;
+    }
 
+    getSalary() {
+        return this.#rate * this.#days;
+    }
 
-
-//!!!!!!!!!!!!exercise3!!!!!!!!!!!!!!
-
-
-const user1 = {
-    firsName:'Vika',
-    lastName:'Rezer',
-    age: 45
 };
 
-const user2 = {
-    firsName:'Aleks',
-    lastName:'Amd',
-    age: 18,
-};
 
-const user3 = {
-    firsName:'Diana',
-    lastName:'Nvidia',
-    age: 5,
-};
-
-const userall = [
-    user1,
-    user2,
-    user3
-];
+user1 = new Worker('Anna', 'Ivanova', 1.5, 22);
 
 
-// const name_key = prompt('Введите ключ...'); 
+console.log(user1);
+console.log(user1.getSalary());
 
-const name_key = 'firsName';
-
-function propertyValue(array, key) {
-    return array.map(item => item[key]);
-}
-
-console.log(propertyValue(userall, name_key));
+user1.rate = 2;
+user1.days = 25; 
 
 
-//!!!!!!!!!!!!exercise4 and exercise6!!!!!!!!!!!!!!
+console.log(user1.rate);
+console.log(user1.days);
+console.log(user1.getSalary());
 
 
-const arrstr = ['Diana', 'Anna', 'Mervyn', 'Jack', 'Ariel', 'Sophie'];
+//_______________exercise 4________________
 
 
-const lengthArrForEach = [];
-
-arrstr.forEach(item => lengthArrForEach.push(item.length));
-console.log('Через forEach');
-console.log(lengthArrForEach);
-
-const lengthArrReduce = arrstr.reduce((acc, item) => ([...acc, item.length]), []);
-console.log('Через reduce');
-console.log(lengthArrReduce);
+//_______________version 1_________________
 
 
-const lengthArrMap = arrstr.map(item => item.length );
-console.log('Через map');
-console.log(lengthArrMap);
+// class Rectangle {
+
+//     constructor (length, width) {
+//         this.length = length;
+//         this.width = width;
+//     }
+
+//     square() {
+//         return this.length*this.width
+//     }
+
+// }
+
+// class Parallelepiped extends Rectangle{
+
+//     constructor (length, width, height) {
+//         super(length, width);
+//         this.height = height;
+//     }
+
+//     size() {
+//         return super.square()*this.height;
+//     }
+
+// };
+
+// figure1 = new Parallelepiped (5, 10, 5);
+
+// console.log(figure1.square());
+// console.log(figure1.size());
 
 
-//!!!!!!!!!!!!exercise5!!!!!!!!!!!!!!
+//_______________version 2_________________
 
 
-const numbers = [2, 3, 5, 7, 11, 13, 17, 19];
+class Parallelepiped {
 
-function currentSums(numbers) {
-    return newNumbers = numbers.reduce((acc, item, i) => {
-        if (i == 0) {
-            acc.push(item);
-            return acc;
-        } else {
-            acc.push(acc[i - 1] + item);
-            return acc;
-        }
-}, []);
-}
-
-
-console.log(currentSums(numbers));
-
-
-//!!!!!!!!!!!!exercise7!!!!!!!!!!!!!!
-
-
-let ver1 = [];
-let ver2 = [];
-
-var myNodelist = document.getElementsByTagName("LI");
-for (let i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-var close = document.getElementsByClassName("close");
-for (let i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
+    constructor (length, width, height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
     }
-}
-
-
-function newElement() {
-    let inputValue = document.getElementById("myInput").value;
-    let inputValuePush;
-    if (inputValue !== '') {
-        if (!/\D/.test(inputValue)) {
-            inputValuePush = +inputValue;
-            ver1.push(inputValuePush);
-        }else {
-            ver2.push(inputValue);
-        }
-    }
-    if (inputValue === '') {
-      alert("Вы должны что-то написать!");
-    }
-    document.getElementById("myInput").value = "";
-    return ver1,ver2;
-}
-
-
-function sorarr() {
-    console.log(ver2);
-    let x = ver1;
-    let y = ver2;
-    let li = document.createElement("li");
-    let li2 = document.createElement("li");
-    let t;
-    let c;
-
-    if (x.length !== 0 && y.length == 0) {
-        x.sort((a, b) => a > b ? 1 : -1)
-        x = x.join(', ');
-        t = document.createTextNode(x);
-        li.appendChild(t);
-        document.getElementById("myUL").prepend(li);
-        document.getElementById("myInput").value = "";
-        ver1 = [];
-
-        var span = document.createElement("SPAN");
-        var txt = document.createTextNode("\u00D7");
-        span.className = "close";
-        span.appendChild(txt);
-        li.appendChild(span);
-      
-        for (i = 0; i < close.length; i++) {
-            close[i].onclick = function() {
-                var div = this.parentElement;
-                div.style.display = "none";
-            }
-        }
-
-        return ver1;
-    } else if (y.length !== 0 && x.length == 0) {
-        y.sort((a, b) => a > b ? 1 : -1)
-        y = y.join(', ');
-        t = document.createTextNode(y);
-        li.appendChild(t);
-        document.getElementById("myUL").prepend(li);
-        document.getElementById("myInput").value = "";
-        ver2 = [];
-
-        var span = document.createElement("SPAN");
-        var txt = document.createTextNode("\u00D7");
-        span.className = "close";
-        span.appendChild(txt);
-        li.appendChild(span);
-      
-        for (i = 0; i < close.length; i++) {
-            close[i].onclick = function() {
-                var div = this.parentElement;
-                div.style.display = "none";
-            }
-        }
-
-        return ver2;
-    } else if (y.length !== 0 && x.length !== 0) {
-        x.sort((a, b) => a > b ? 1 : -1)
-        x = x.join(', ');
-        y.sort((a, b) => a > b ? 1 : -1)
-        y = y.join(', ');
-        t = document.createTextNode(x);
-        c = document.createTextNode(y);
-        li.appendChild(t);
-        li2.appendChild(c);
-        document.getElementById("myUL").prepend(li);
-        document.getElementById("myUL").prepend(li2);
-        document.getElementById("myInput").value = "";
-        ver1 = [];
-        ver2 = [];
-
-        var span = document.createElement("SPAN");
-        var span2 = document.createElement("SPAN");
-        var txt = document.createTextNode("\u00D7");
-        var txt2 = document.createTextNode("\u00D7");
-        span.className = "close";
-        span2.className = "close";
-        span.appendChild(txt);
-        span2.appendChild(txt2);
-        li.appendChild(span);
-        li2.appendChild(span2);
     
-        for (i = 0; i < close.length; i++) {
-            close[i].onclick = function() {
-                var div = this.parentElement;
-                div.style.display = "none";
-            }
-        }
-
-        return ver1, ver2;
+    size() {
+        return this.length * this.width * this.height
     }
+
 }
+
+class Rectangle extends Parallelepiped{
+
+    constructor (length, width, height) {
+        super(length, width, height);
+    }
+
+    square() {
+        return this.length * this.width
+    }
+
+};
+
+figure1 = new Rectangle (5, 10, 5);
+
+console.log(figure1);
+
+console.log(figure1.square());
+console.log(figure1.size());
