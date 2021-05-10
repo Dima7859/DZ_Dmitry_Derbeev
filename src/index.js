4d2860a89c9706ebc997d325c4c7422f0da0a559
Dima7859 <68110092+Dima7859@users.noreply.github.com>
2021-05-10 19:25:52 +0300

    DZ_20

--- src/index.js
+++ src/index.js
@@ -1,97 +1,78 @@
-import '../style/style.scss'
 
-const inp = document.getElementById('inp');
-const form = document.querySelector('form');
-const add = document.getElementById('add');
-const clearAll = document.getElementById('clearAll');
+let todos;
+const btnTrue = document.getElementById('btnTrue');
+const btnNotTrue = document.getElementById('btnNotTrue');
+const btnAll = document.getElementById('btnAll');
 
-let allArr = localStorage.getItem('inp') ? JSON.parse(localStorage.getItem('inp')) : [];
-
-localStorage.setItem('inp', JSON.stringify(allArr));
-const data = JSON.parse(localStorage.getItem('inp'));
-
+const block = document.getElementById('block');
 const ul = document.createElement('ul');
-document.body.append(ul);
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    if (inp.value === '') {
-        alert("Вы должны что-то написать!");
-    } else {
-        allArr.push(inp.value);
-        localStorage.setItem('inp', JSON.stringify(allArr));
-        liMaker(inp.value);
-        inp.value = "";
-    }
-});
-
-add.onclick = () => {
-    if (inp.value === '') {
-        alert("Вы должны что-то написать!");
-    } else {
-        allArr.push(inp.value);
-        localStorage.setItem('inp', JSON.stringify(allArr));
-        liMaker(inp.value);
-        inp.value = "";
-    }
-};
-
-const liMaker = (text) => {
-    document.getElementById('addsound').play();
-    const li = document.createElement('li');
-    li.textContent = text;
-    ul.appendChild(li);
-
-    const btn = document.createElement('input');
-    btn.className = 'inpbtn';
-    btn.getAttribute('type')
-    btn.setAttribute('type', "image");
-    btn.getAttribute('src')
-    btn.setAttribute('src', "../img/red.png");
-    li.append(btn)
-
-    btn.onclick = () => {
-        let contli = li.textContent;
-        document.getElementById('sound').play();
-        let arr = []
-        allArr.forEach((item) => {
-            if (item !== contli) {
-                arr.push(item);
-            }
-        })
-        allArr = arr;
-        arr = [];
-        btn.remove();
-        li.remove();
-        localStorage.setItem('inp', JSON.stringify(allArr));
-    }
-
-    li.onclick  = () => {
-        const isClicked = li.getAttribute('clicked');
-        if (!isClicked) {
-            li.setAttribute('clicked', true);
-            li.style.textDecoration = 'line-through';
-            li.style.textDecorationColor = 'red';
-        } else {
-            li.removeAttribute('clicked');
-            li.style.textDecoration = 'none';
-        }
-    }
-
-};
-
-data.forEach(item => {
-    liMaker(item);
-});
+block.prepend(ul);
 
 
-clearAll.onclick = () => {
-    localStorage.clear();
+function renderItems(ittd ,var1) {
+    let td = ittd;
+    const b = var1;
+    let tdInterim = [];
+
+
     while (ul.firstChild) {
         ul.removeChild(ul.firstChild);
     }
-    allArr = [];
-    inp.value = "";
-    document.getElementById('sound').play();
+
+    if (b !== undefined) {
+        td.forEach(item => {
+            if (item.completed === b) {
+                tdInterim.push(item);
+            }
+        });
+        td = tdInterim;
+        tdInterim = [];
+    }
+
+    td.forEach(item => {
+    const li = document.createElement('li');
+    li.innerHTML = item.title;
+    ul.appendChild(li);
+    li.onclick  = () => {
+        const isClicked = li.getAttribute('clicked');
+        if (!isClicked) {
+            document.getElementById('addsound').play();
+            li.setAttribute('clicked', true);
+            li.style.color = 'red';
+            li.style.fontSize = '50px';
+        } else {
+            document.getElementById('sound').play();
+            li.removeAttribute('clicked');
+            li.removeAttribute("style")
+        }
+    };
+    });
+};
+
+const fetchtd = () => {
+    return fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(json => todos = json);
+};
+
+const getTodos = async () => {
+    await fetchtd();
+    renderItems(todos);
+}
+
+getTodos();
+
+btnTrue.onclick = () => {
+    document.getElementById('tudu').play();
+    renderItems(todos, true);
+};
+
+btnNotTrue.onclick = () => {
+    document.getElementById('tudu').play();
+    renderItems(todos, false);
+};
+
+btnAll.onclick = () => {
+    document.getElementById('tudu').play();
+    renderItems(todos);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { prototype } = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'budle.js',
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
-        new HTMLWebpackPlugin({
+        new HTMLWebpackPlugin ({
             template: './src/index.html'
+
         }),
         new CleanWebpackPlugin()
     ],
@@ -18,7 +20,7 @@ module.exports = {
         rules: [
             {
                 test: /\.scss$/,
-                use:['style-loader', 'css-loader', 'sass-loader']
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
